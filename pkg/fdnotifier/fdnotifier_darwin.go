fa30e6054a30f8d78c13b01b3d4e50e64f94931e
Walter Scott <w@lter.ca>
2025-05-04 08:36:34 -0500

    add fdnotifier and rawfile files for darwin

---
+++ pkg/fdnotifier/fdnotifier_darwin.go
@@ -0,0 +1,7 @@
+//go:build darwin
+
+package fdnotifier
+
+// This file is intentionally left blank initially. It satisfies the Go build
+// system for the darwin target. If specific functions/types are needed, they
+// will be added here as stubs (e.g., a stub implementation of Queue).
---
+++ pkg/rawfile/rawfile_darwin.go
@@ -0,0 +1,7 @@
+//go:build darwin
+
+package rawfile
+
+// This file is intentionally left blank initially. It satisfies the Go build
+// system for the darwin target. If specific functions are needed, they will
+// be added here as stubs.
